d13ce1d5f0592d3fb4b2cce88a1506c7f51feb44
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2025-06-30 11:08:15 +0530

    Time: 7 ms (18.29%), Space: 45 MB (6.1%) - LeetHub

--- 1676-minimum-number-of-days-to-eat-n-oranges/1676-minimum-number-of-days-to-eat-n-oranges.java
+++ 1676-minimum-number-of-days-to-eat-n-oranges/1676-minimum-number-of-days-to-eat-n-oranges.java
@@ -2,7 +2,7 @@ class Solution {
     static Map<Integer,Integer> cache = new HashMap();
     public int minDays(int n) {
 
-        return minDays2(n);
+        return minDays1(n);
     }
 
     public int minDays2(int n){
@@ -22,16 +22,18 @@ class Solution {
         int one=Integer.MAX_VALUE;
         int two=Integer.MAX_VALUE;
         int three = Integer.MAX_VALUE;
+        if(n%2!=0 || n%3!=0)one = minDays1(n-1);
         if(n%3==0){
             three = minDays1(n-(2*(n/3)));
         }
         if(n%2==0){
             two = minDays1(n-(n/2));
         }
-        if(n%2!=0 || n%3!=0)one = minDays1(n-1);
         int min = Math.min(one,Math.min(two,three));
         int num = 1+(min ==Integer.MAX_VALUE ? 0 : min);
         cache.put(n,num);
         return num;
     }
 }
+
+
