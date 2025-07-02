d40293faab785817164e0228c66072a860b1f667
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2025-07-02 14:12:15 +0530

    Time: 0 ms (100%), Space: 41 MB (7.61%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,28 @@
+class Solution {
+    int[] dp;
+    public int integerBreak(int n) {
+       dp=new int[n+1];
+       return integerBreak2(n,0);
+    }
+
+    public int integerBreak1(int n){
+        if(n<=1)return 1;
+        if(dp[n]!=0)return dp[n];
+        int product = 1;
+        for(int i=1;i<n;i++){
+          product = Math.max(product,integerBreak1(n-i)*i);
+        }
+        return dp[n]=product;
+    }
+
+    public int integerBreak2(int n, int sum){
+        if(sum==n) return 1;
+        if(sum>n)return 0;
+        if(dp[sum]!=0)return dp[sum];
+        int product = 1;
+        for(int i=1;i<n;i++){
+            product = Math.max(product,integerBreak2(n,sum+i)*i);
+        }
+        return dp[sum]=product;
+    }
+}
