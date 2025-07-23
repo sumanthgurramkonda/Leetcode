5b21416a8b06f7d81a686c61c56cee1776daf1b1
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2025-07-23 23:51:33 +0530

    Time: 1 ms (100%), Space: 42.4 MB (75.53%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -0,0 +1,55 @@
+class Solution {
+    
+    public int orangesRotting(int[][] grid) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+        int fresh = 0;
+        Queue<int[]> rottenQueue = new LinkedList();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    rottenQueue.offer(new int[]{i,j});
+                }
+                if(grid[i][j]==1)fresh+=1;
+            }
+        }
+        if(fresh==0)return 0;
+        int minTime = 0;
+        while(!rottenQueue.isEmpty()){
+            int size = rottenQueue.size();
+            if(fresh!=0)minTime++;
+            for(int k=0;k<size;k++){
+                int[] arr = rottenQueue.poll();
+                int i = arr[0], j = arr[1];
+                if(i-1>=0 && grid[i-1][j]==1){
+                    rottenQueue.offer(new int[]{i-1,j});
+                    fresh--;
+                    grid[i-1][j]=2;
+                }
+                if(i+1<n && grid[i+1][j]==1){
+                    rottenQueue.offer(new int[]{i+1,j});
+                    fresh--;
+                    grid[i+1][j]=2;
+                }
+                if(j-1>=0 && grid[i][j-1]==1){
+                    rottenQueue.offer(new int[]{i,j-1});
+                    fresh--;
+                    grid[i][j-1]=2;
+                }
+                if(j+1<m && grid[i][j+1]==1){
+                    rottenQueue.offer(new int[]{i,j+1});
+                    fresh--;
+                    grid[i][j+1]=2;
+                }
+            }
+        }
+        return fresh==0 ? minTime : -1;
+    }
+
+}
+
+  
+
+
