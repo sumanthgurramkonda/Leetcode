9057eabbd669e0b5d966442d6c958b91d6b7d307
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2025-07-28 19:24:16 +0530

    Time: 46 ms (19.21%), Space: 49.9 MB (24.44%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -0,0 +1,55 @@
+class Solution {
+    Map<String,String> mailToName = new HashMap();
+    Map<String,String> childToParent = new HashMap();
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+
+        for(List<String> mails : accounts){
+            String name = mails.get(0);
+            for(int i=1;i<mails.size();i++){
+                childToParent.put(mails.get(i),mails.get(i));
+                mailToName.put(mails.get(i),name);
+            }
+        }
+        
+        for(List<String> mails : accounts){
+            String firstMail = mails.get(1);
+            for(int i=1;i<mails.size();i++){
+                union(firstMail,mails.get(i));
+            }
+        }
+        Map<String, Set<String>> result = new HashMap();
+        childToParent.forEach((child,parent)->{
+            parent = find(child);
+            result.putIfAbsent(parent,new TreeSet());
+            result.get(parent).add(child);
+        });
+        List<List<String>> resultList = new ArrayList();
+        result.forEach((mail,set)->{
+            List<String> list = new  ArrayList();
+            list.add(mailToName.get(mail));
+            for(String mail1 : set){
+                list.add(mail1);
+            }
+            resultList.add(list);
+        });
+        return resultList;
+    }
+
+
+    public String find(String email){
+        String parent = childToParent.get(email);
+        if(!parent.equals(email)){
+            childToParent.put(email,find(parent));
+        }
+        return childToParent.get(email);
+    }
+
+    public void union(String firstMail,String mail){
+        String parent1 = find(firstMail);
+        String parent2 = find(mail);
+        if(!parent1.equals(parent2)){
+            childToParent.put(parent2, parent1);
+        }
+    }
+}
+
