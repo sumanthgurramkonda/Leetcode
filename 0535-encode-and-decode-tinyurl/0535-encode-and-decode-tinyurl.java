6d7a04410b0623c4ef426053caa6a0b6c75f8a23
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2025-09-10 20:41:58 +0530

    Time: 0 ms (100%), Space: 43.2 MB (45.97%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -2,12 +2,12 @@ public class Codec {
 
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        return longUrl+1;
+        return longUrl;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl.substring(0,shortUrl.length()-1);
+        return shortUrl;
     }
 }
 
