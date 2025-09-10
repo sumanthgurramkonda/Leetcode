d8e364ded86698bd1b80f046c19a2ef43bb75484
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2025-09-10 20:55:32 +0530

    Time: 2 ms (70.49%), Space: 42.6 MB (92.57%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -1,13 +1,23 @@
 public class Codec {
 
-    // Encodes a URL to a shortened URL.
+    String chars = "01fgh23489abcdei567jklQRSTUVWXYZmnopqwxyzABCDEFGrstuvHIJKLMNOP";
+    Map<String, String> db = new HashMap<>();
+    public String makeTinyUrl(){
+        StringBuilder str = new StringBuilder("https://tinyUrl.com/");
+        for(int i = 0;i<6;i++){
+            str.append(chars.charAt((int)(Math.random() * 62)));
+        }
+        return str.toString();
+    }
     public String encode(String longUrl) {
-        return longUrl;
+        String key = makeTinyUrl();
+        db.put(key,longUrl);
+        return key;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl;
+        return db.get(shortUrl);
     }
 }
 
