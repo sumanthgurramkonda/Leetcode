b4f9be45ba8438ef62aff1237ffc5b0c9af18f5f
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2025-09-22 22:37:45 +0530

    Time: 0 ms (100%), Space: 44.6 MB (64.31%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,38 +2,9 @@ class Solution {
 
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList(nums.length);
-        // permutations(nums,0,res);
         permutations(nums,0,res);
         return res;
     }
-
-    public void permutation(int[] nums, int index, List<List<Integer>> res){
-        if(index==nums.length-1){
-            List<Integer> list = new ArrayList();
-            for(int n : nums)list.add(n);
-            res.add(list);
-        }
-
-        for(int i=0;i<nums.length;i++){
-            permutation(nums,index+1,res);
-            int temp = nums[index];
-            nums[index]=nums[index+1];
-            nums[index+1]=temp;
-            permutation(nums,index+1,res);
-            temp = nums[index];
-            nums[index]=nums[index+1];
-            nums[index+1]=temp;
-        }
-
-
-    }
-
-
-
-
-
-
-
 
     public void permutations(int[] nums,int i,List<List<Integer>> res){
 
