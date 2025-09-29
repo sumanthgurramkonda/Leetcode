16f9b0c5a1c7f477569929153fc81457f146c37e
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2025-09-29 21:10:09 +0530

    Time: 35 ms (23.99%), Space: 63.4 MB (62.84%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,24 +1,51 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int count=1;
-		int maxCount = 0;
-		Set<Integer> set = new HashSet<Integer>();
-		for(int i : nums)set.add(i);
-		Set<Integer> set1 = new HashSet<Integer>();
-		Iterator iterator = set.iterator();	
+        // int count=1;
+		// int maxCount = 0;
+		// Set<Integer> set = new HashSet<Integer>();
+		// for(int i : nums)set.add(i);
+		// Set<Integer> set1 = new HashSet<Integer>();
+		// Iterator iterator = set.iterator();	
 		
-		while(iterator.hasNext()) {
-			int val = (int)iterator.next();
-			if(!set1.contains(val-1)) {
-				set1.add(val);
-				while(set.contains(++val)) {
-					count++;
-					set1.add(val);
-				}
-			}
-			maxCount = Math.max(maxCount, count);
-			count=1;
-		}
-		return maxCount;
+		// while(iterator.hasNext()) {
+		// 	int val = (int) iterator.next();
+		// 	if(!set1.contains(val-1)) {
+		// 		set1.add(val);
+		// 		while(set.contains(++val)) {
+		// 			count++;
+		// 			set1.add(val);
+		// 		}
+		// 	}
+		// 	maxCount = Math.max(maxCount, count);
+		// 	count=1;
+		// }
+		// return maxCount;
+        return sol(nums);
+    }
+
+    public int sol(int[] nums){
+
+        Set<Integer> set = new HashSet();
+        for(int n : nums)set.add(n);
+        int res = 0;
+        for(int num : nums){
+            int count = 0;
+            if(set.contains(num)){
+                int n = num;
+                while(set.contains(n)){
+                    set.remove(n);
+                    n++;
+                    count++;
+                }
+                n = num-1;
+                while(set.contains(n)){
+                    set.remove(n);
+                    n--;
+                    count++;
+                }
+            }
+            res = Math.max(res,count);
+        }
+        return res;
     }
 }
