b84018ca31c33d367ba53f8339e42a54adecdd7e
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2025-10-30 19:13:15 +0530

    Time: 337 ms (41.33%), Space: 40.9 MB (60.46%) - LeetHub

---
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    int[] cookiesSum;
+    int max=0;
+    int min=Integer.MAX_VALUE;
+    
+    public int distributeCookies(int[] cookies, int k) {
+        cookiesSum = new int[k];
+        return dfs(cookies,0);
+    }
+
+    public int dfs(int[] cookies, int index){
+        if(index==cookies.length){
+            int maxValue = Integer.MIN_VALUE;
+            for(int val : cookiesSum){
+                maxValue = Math.max(val, maxValue);
+            }
+            return maxValue;
+        }
+        int minSum = Integer.MAX_VALUE;
+        for(int i=0;i<cookiesSum.length;i++){
+            cookiesSum[i]+=cookies[index];
+            minSum= Math.min(minSum,dfs(cookies, index+1));
+            cookiesSum[i]-=cookies[index];
+        }
+        return minSum;
+    }
+}
+
+
