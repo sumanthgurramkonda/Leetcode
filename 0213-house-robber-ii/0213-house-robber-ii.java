b2a976c266ea72f9378148dd46a96f9ad7d59388
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2025-11-12 22:39:59 +0530

    Time: 0 ms (100%), Space: 42.9 MB (5.21%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,19 +1,22 @@
 class Solution {
+    int[] dp;
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n<=1)return nums[0];
-       
-        return Math.max(rob(nums,0,1,n-1),rob(nums,1,2,n));
+        if(n==1)return nums[0];
+        dp = new int[n];
+        Arrays.fill(dp,-1);
+        int first = dfs(nums,0,n-1);
+        Arrays.fill(dp,-1);
+        int second = dfs(nums,1,n);
+        return Math.max(first,second);
     }
-    public int rob(int nums[],int k,int m,int n){
-        int prevRow = 0;
-        int maxRow = nums[k];
-        for(int i=m;i<n;i++){
-            int temp = Math.max(prevRow+nums[i],maxRow);
-            prevRow = maxRow;
-            maxRow = temp; 
-        }
-        return Math.max(prevRow,maxRow);
+
+    public int dfs(int[] nums, int index, int n){
+        if(index>=n)return 0;
+        if(dp[index]!=-1)return dp[index];
+        int skip = nums[index]+dfs(nums,index+2,n);
+        int noSkip = dfs(nums, index+1,n);
+        return dp[index]=Math.max(skip, noSkip);
     }
 
 }
