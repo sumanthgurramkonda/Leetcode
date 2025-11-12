10318788dcb801e69346f40af14525cdaaaeafba
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2025-11-12 22:48:07 +0530

    Time: 0 ms (100%), Space: 42.9 MB (5.21%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,22 +1,51 @@
+// class Solution {
+//     int[] dp;
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         if(n==1)return nums[0];
+//         dp = new int[n];
+//         Arrays.fill(dp,-1);
+//         int first = dfs(nums,0,n-1);
+//         Arrays.fill(dp,-1);
+//         int second = dfs(nums,1,n);
+//         return Math.max(first,second);
+//     }
+
+//     public int dfs(int[] nums, int index, int n){
+//         if(index>=n)return 0;
+//         if(dp[index]!=-1)return dp[index];
+//         int skip = nums[index]+dfs(nums,index+2,n);
+//         int noSkip = dfs(nums, index+1,n);
+//         return dp[index]=Math.max(skip, noSkip);
+//     }
+
+// }
+
 class Solution {
     int[] dp;
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n==1)return nums[0];
         dp = new int[n];
         Arrays.fill(dp,-1);
         int first = dfs(nums,0,n-1);
         Arrays.fill(dp,-1);
         int second = dfs(nums,1,n);
-        return Math.max(first,second);
+        int max = Math.max(first,Math.max(nums[n-1],second));
+        Arrays.fill(dp,-1);
+        for(int i=2;i<n;i++){
+            max = Math.max(max,dfs(nums,i,n));
+        }
+        return max;
     }
 
     public int dfs(int[] nums, int index, int n){
         if(index>=n)return 0;
         if(dp[index]!=-1)return dp[index];
-        int skip = nums[index]+dfs(nums,index+2,n);
-        int noSkip = dfs(nums, index+1,n);
-        return dp[index]=Math.max(skip, noSkip);
+        int max = 0;
+        for(int i=index+2;i<n;i++){
+            max = Math.max(max, dfs(nums,i,n));
+        }
+        return dp[index]=nums[index]+max;
     }
-
+    
 }
