061bfff9d943b403293019d8c7fa96646514f043
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-01-19 22:18:14 +0530

    Time: 15 ms (61.75%), Space: 51.9 MB (11.94%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+        int i=0;
+        for(int j=0;i<g.length && j<s.length;j++){
+            if(s[j]>=g[i]){
+                count++;
+                i++;
+            }
+        }
+        return count;
+    }
+}
