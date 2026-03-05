57b77c9929f69969a432bfb6851244e418f56f80
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-03-05 11:20:42 +0530

    Time: 1 ms (96.81%), Space: 46.3 MB (71.44%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,25 +13,24 @@
  *     }
  * }
  */
- 
 class Solution {
-    Map<Integer, Integer> map = new HashMap();
-    int index=0;
+    
+    Map<Integer, Integer> indexMap = new HashMap<>();
+    int index = 0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         int n = inorder.length;
-        for(int i=0;i<n;i++){
-            map.put(inorder[i],i);
-        }
-        return buildTree(preorder,0,n-1);
-    }
-    public TreeNode buildTree(int[] preorder,int l,int r) {
-        if(l>r) return null;
-        int rootVal = preorder[index++];
-        TreeNode root = new TreeNode(rootVal);
-        int mid = map.get(rootVal);
+        for(int i=0;i<n;i++)indexMap.put(inorder[i],i);
 
-        root.left = buildTree(preorder,l,mid-1);
-        root.right = buildTree(preorder,mid+1,r);
-        return root;
+        return dfs(preorder, 0,n-1);
+        
+    }
+
+    public TreeNode dfs(int[] preorder, int l, int r){
+        if(l>r)return null;
+        int root = preorder[index++];
+        int mid = indexMap.get(root);
+        TreeNode left = dfs(preorder, l,mid-1);
+        TreeNode right = dfs(preorder, mid+1,r);
+        return new TreeNode(root,left,right);
     }
 }
