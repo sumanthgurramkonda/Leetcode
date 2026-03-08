9503c9ff8772ce6680efda67033e85c3183647f0
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-03-09 01:09:58 +0530

    Time: 8 ms (20.71%), Space: 47.8 MB (24.92%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,67 +1,44 @@
 class Solution {
-    Map<Integer, List<Integer>> adjList =new HashMap();
+    Map<Integer, List<Integer>> adjList = new HashMap<>();
     boolean[] visited;
     boolean[] path;
-    // List<Integer> res = new LinkedList();
-    Set<Integer> res = new LinkedHashSet();
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if(numCourses==0)return null;
         if(prerequisites.length==0){
-            int[] res = new int[numCourses];
-            for(int i=0;i<numCourses;i++){
-                res[i]=numCourses-1;
-                numCourses-=1;
+            int[] arr = new int[numCourses];
+            for(int i=0;i<prerequisites.length;i++){
+                arr[i] = prerequisites.length-i-1;
+                return arr;
             }
-            return res;
         }
-        path = new boolean[numCourses];
         visited = new boolean[numCourses];
-
-        for(int i=0;i<prerequisites.length;i++){
-            int a = prerequisites[i][0];
-            int b = prerequisites[i][1];
-            if(adjList.containsKey(a)){
-                adjList.get(a).add(b);
-            }else{
-                List<Integer> list = new ArrayList();
-                list.add(b);
-                adjList.put(a,list);
-            }
-        }
-        boolean isVisited= false;
+        path = new boolean[numCourses];
         for(int i=0;i<numCourses;i++){
-            if(dfs(i)) {
-                return new int[0];
-            }
-            else res.add(i);
+            adjList.put(i, new ArrayList<>());
         }
-        int[] resArray = new int[res.size()];
-        // for(int i=0;i<res.size();i++) resArray[i]=res.get(i);
-        int i = 0;
-        for(int n : res){
-            resArray[i++]=n;
+        for(int[] arr : prerequisites){
+            adjList.get(arr[0]).add(arr[1]);
         }
-        return resArray;
+        for(int i=0;i<numCourses;i++){
+            if(dfs(i))return new int[0];
+        }
+
+        if(res.size()<numCourses)return null;
+        return res.stream().mapToInt(c -> c).toArray();
     }
 
+    List<Integer> res = new ArrayList<>();
+    
     public boolean dfs(int node){
-        if(path[node]) return true;
+        if(path[node])return true;
         if(visited[node])return false;
-        List<Integer> list = adjList.get(node);
-        if(list==null || list.size()==0)return false;
-        visited[node] = true;
         path[node]=true;
-        for(int n : list){
-            if(dfs(n)){
-                return true;
-            }else{
-                res.add(n);
-            }
+        visited[node]=true;
+        for(int n : adjList.get(node)){
+            if(dfs(n)) return true;
         }
+        res.add(node);
         path[node]=false;
         return false;
     }
 }
-
-
-
-
