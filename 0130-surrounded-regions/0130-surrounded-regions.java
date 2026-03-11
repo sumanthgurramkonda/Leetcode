f21e82d5e9b73212747f3ded838ea420afe45fbd
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-03-11 15:11:30 +0530

    Time: 2 ms (80.4%), Space: 48.1 MB (20.38%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,52 +1,32 @@
 class Solution {
     public void solve(char[][] board) {
 
-        char c = '1';
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]=='O' && (i==0 || i==board.length-1||j==0||j==board[i].length-1)){
-                    dfs(board,i,j,c);
-                }
-            }
+        int n = board.length;
+        int m = board[0].length;
+        for(int i=0;i<n;i++){
+            dfs(board,i,0);
+            dfs(board,i,m-1);
         }
-        // for(int i=0;i<board.length;i++){
-        //     for(int j=0;j<board[0].length;j++){
-        //         if(board[i][j]=='O'){
-        //             dfs(board,i,j,'X');
-        //         }
-        //     }
-        // }
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]==c){
-                    board[i][j]='O';
-                }
-                else if(board[i][j]=='O'){
-                    // dfs(board,i,j,'X');
-                    board[i][j]='X';
-                }
+        for(int i=0;i<m;i++){
+            dfs(board,0,i);
+            dfs(board,n-1,i);
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O') board[i][j]='X';
+                if(board[i][j]=='1') board[i][j]='O';
             }
         }
         
     }
-    private class Pair{
-        int first;
-        int second;
-        Pair(int first,int second){
-            this.first = first;
-            this.second = second;
+    int[][] directions = {{1,0}, {-1,0}, {0,1},{0,-1}};
+    public void dfs(char[][] board, int r, int c){
+        if(r<0 || r>=board.length || c<0 || c>= board[0].length || board[r][c]!='O')return;
+        board[r][c]='1';
+        for(int[] dir : directions){
+            int i = r+dir[0];
+            int j = c+dir[1];
+            dfs(board,i,j);
         }
     }
-    public void dfs(char[][] board,int i,int j,char cell){
-        board[i][j]=cell;
-        int arr[][] = {{-1,0},{0,-1},{1,0},{0,1}};
-        for(int k=0;k<4;k++){
-            int r = i+arr[k][0];
-            int c = j+arr[k][1];
-            if(r>=0 && r<board.length && c>=0 && c<board[0].length && board[r][c]=='O'){
-                dfs(board,r,c,cell);
-            }
-        }
-
-    }
-}
+} 
