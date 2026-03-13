4ecc41466f188636bad4372bf1cfe9d7dad2e405
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-03-13 12:47:12 +0530

    Time: 4 ms (5.43%), Space: 43.6 MB (14.71%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,32 +1,28 @@
 class Solution {
-    
+    int[] dp;
     public int numDecodings(String s) {
-        if(s.charAt(0)=='0') return 0;
-        int[] dp = new int[s.length()];
+        dp = new int[s.length()];
         Arrays.fill(dp,-1);
-        return numDecoding(s.toCharArray(), 1, 0,dp);
+        return dfs(s,-1, 0);   
     }
 
-    public int numDecoding(char[] s, int num, int i,int[] dp){
+    public int dfs(String s, int num, int i){
         if(num==0)return 0;
-        if(i>=s.length){
-            if(num>=1 && num<=26) return 1;
-            return 0;
-        }
+        if(i==s.length())return 1;
         if(dp[i]!=-1)return dp[i];
-        num = s[i]-'0';
-        int counter1 = numDecoding(s,num,i+1,dp);
-        int counter2 = 0;
-        if(i<=s.length-2){
-            num = s[i]-'0';
-            num*=10;
-            num+=(s[i+1]-'0');
-            if(num>=10 && num<=26)counter2 = numDecoding(s,num,i+2,dp);
+        char c1 = s.charAt(i);
+        if(c1=='0')return 0;
+        int oneDigitCount = dfs(s,Integer.parseInt(""+c1),i+1);
+        int twoDigitCount = 0;
+        if(i+1<s.length()){
+            char c2 = s.charAt(i+1);
+            int number = Integer.parseInt(""+c1+c2);
+            if(number>=10 && number<=26)
+                twoDigitCount = dfs(s,number,i+2);
         }
-        return dp[i] = counter1+counter2;
-    }
+        return dp[i] = oneDigitCount + twoDigitCount;
+    }   
 }
-
 
 
 
