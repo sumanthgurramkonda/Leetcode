b3539b235fb756e4e2624f464b9b3242b7de2f56
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-03-14 20:24:53 +0530

    Time: 6 ms (86.49%), Space: 46.2 MB (51.63%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,26 +1,23 @@
 class Solution {
-    int[] dp;
     public int lengthOfLIS(int[] nums) {
-        dp = new int[nums.length+1];
-        return lengthOfLIS1(nums);
-    }
 
-    public int lengthOfLIS1(int[] nums){
-        int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,1);
-        int res = 1;
-        for(int i=0;i<n-1;i++){
-            int len = 0;
-            for(int j=i+1;j<n;j++){
-                if(nums[i]<nums[j]){
-                    dp[j] = Math.max(dp[j],dp[i]+1);
+        List<Integer> sortedNumsList = new ArrayList<>();
+
+        for(int num : nums){
+            int left = 0;
+            int right = sortedNumsList.size();
+           
+            while(left<right){
+                int mid = left + (right-left)/2;
+                if(sortedNumsList.get(mid)<num){
+                    left = mid+1;
+                }else{
+                    right = mid;
                 }
             }
+            if(left==sortedNumsList.size())sortedNumsList.add(num);
+            else sortedNumsList.set(left, num);
         }
-        for(int i=0;i<n;i++){
-            res = Math.max(res,dp[i]);
-        }
-        return res;
+        return sortedNumsList.size();
     }
 }
