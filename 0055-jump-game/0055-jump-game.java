34bf2f75680df490bd8a3c4ca4bdcc643e4024fd
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-03-16 19:54:26 +0530

    Time: 2 ms (85.44%), Space: 47.8 MB (88.7%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,8 +2,16 @@ class Solution {
 
     Boolean[] visited;
     public boolean canJump(int[] nums) {
-        visited = new Boolean[nums.length];
-        return canJump(nums,0);
+        // visited = new Boolean[nums.length];
+
+        int max = 0;
+        for(int i=0;i<nums.length-1;i++){
+            max = Math.max(max,nums[i]+i);
+            if(max==i)return false;
+            if(max>=nums.length)return true;
+        }
+        return max>=nums.length-1;
+        // return canJump(nums,0);
     }
 
     public boolean canJump(int[] nums, int index){
