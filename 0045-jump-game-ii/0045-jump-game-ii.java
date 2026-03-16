4f94ae7d02f1bd5ab06164f4461449a531f0422e
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-03-16 20:58:44 +0530

    Time: 117 ms (7.26%), Space: 47.6 MB (16.12%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,55 +1,20 @@
 class Solution {
     int[] dp;
     public int jump(int[] nums) {
-
-        // dp = new int[nums.length];
-        // Arrays.fill(dp,-1);
-        // // return dfs(nums,dp,0);
-        // int steps = dfs1(nums,0);
-        // return steps == Integer.MAX_VALUE ? -1 : steps;
-        return minJump(nums);
+        dp = new int[nums.length];
+        Arrays.fill(dp,-1);
+        return jump(nums,0);
     }
 
-    public int minJump(int[] nums){
-        int steps = 0;
-        int farthestStep = 0;
-        int currentStep = 0;
-        for(int i=0;i<nums.length-1;i++){
-            farthestStep = Math.max(farthestStep, nums[i]+i);
-            if(i==currentStep){
-                currentStep = farthestStep;
-                steps++;
-            }
-            if(currentStep>=nums.length-1)return steps;
-        }
-        return steps;
-    }
-
-    public int dfs1(int[] nums, int index){
-        if(index>=nums.length) return Integer.MAX_VALUE;
-        if(index==nums.length-1){
-            return 0;
-        }
+    public int jump(int[] nums, int index){
+        if(index>= nums.length-1) return 0;
         if(dp[index]!=-1)return dp[index];
-        int minJumps = Integer.MAX_VALUE;
-        for(int i=1;i<=nums[index];i++){
-            int count = dfs1(nums,index+i);
-            if(count==Integer.MAX_VALUE)continue;
-            minJumps = Math.min(count+1,minJumps);
+        int minSteps = Integer.MAX_VALUE;
+        for(int i=index+1;i<=index+nums[index] && i<nums.length;i++){
+            // if(nums[i]==0)continue;
+            minSteps = Math.min(minSteps,jump(nums,i));
         }
-        return dp[index]=minJumps;
-    }
-
-
-    public int dfs(int[] nums, int[] dp, int index){
-        if(index>=nums.length-1)return 0;
-        if(nums[index]==0)return Integer.MAX_VALUE;
-        if(dp[index]!=-1)return dp[index];
-        int min = Integer.MAX_VALUE;
-        for(int i=1;i<=nums[index];i++){
-            int jump = dfs(nums,dp,index+i);
-            if(jump!=Integer.MAX_VALUE)min = Integer.min(jump+1, min);
-        }
-        return dp[index]=min;
+        return dp[index] =  minSteps==Integer.MAX_VALUE ? Integer.MAX_VALUE : minSteps+1;
     }
 }
+
