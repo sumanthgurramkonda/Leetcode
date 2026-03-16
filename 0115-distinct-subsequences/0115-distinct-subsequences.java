e0f99cc39e5bb1a50c94cafec3fe6a0fcdb61dad
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-03-17 00:00:18 +0530

    Time: 24 ms (13.58%), Space: 54.7 MB (25.71%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,29 +1,23 @@
 class Solution {
+    int[][] dp;
     public int numDistinct(String s, String t) {
-        int dp[][] = new int[s.length()][t.length()];
-        for(int[] arr : dp)Arrays.fill(arr,-1);
-        return distinctSubSequences(s,t,0,0,dp);
+        dp = new int[s.length()][t.length()];
+        for(int[] arr : dp) Arrays.fill(arr,-1);
+        return numDistinct(s,t,0,0);
     }
 
-    public int distinctSubSequences(String s, String t, int i, int j, int dp[][]){
-        if(j>=t.length()) return 1;
-        if(i>=s.length())return 0;
+    public int numDistinct(String s, String t, int i, int j){
+        if(j==t.length())return 1;
+        if(i==s.length())return 0;
         if(dp[i][j]!=-1)return dp[i][j];
+        int count = 0;
         if(s.charAt(i)==t.charAt(j)){
-            int count1 = 0;
-            count1 += distinctSubSequences(s,t,i+1,j+1,dp);
-            int count2 = 0;
-            count2 += distinctSubSequences(s,t,i+1,j,dp);
-            dp[i][j] = count1 + count2;
+            count = numDistinct(s,t,i+1,j+1);
+            count += numDistinct(s,t,i+1,j);
+        }else{    
+            count = numDistinct(s,t,i+1,j);
         }
-        else {
-            int count = 0;
-            count += distinctSubSequences(s,t,i+1,j,dp);
-            dp[i][j]=count;
-        }
-        return dp[i][j];
+
+        return dp[i][j]=count;
     }
 }
-
-
-
