d6eca39f2b9472965000f934026d4710e1c9066c
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-03-16 22:31:52 +0530

    Time: 121 ms (18.55%), Space: 42.5 MB (97.67%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,17 +1,49 @@
 class Solution {
-    
-    Map<Integer, Integer> map = new HashMap<>();
-
+    int res=Integer.MAX_VALUE;
     public int numSquares(int n) {
-        if(n==0)return 0;
-        if(n<0)return Integer.MAX_VALUE;
-        if(map.containsKey(n))return map.get(n);
-        int minCount = Integer.MAX_VALUE;
-        for(int i=1;i*i<=n;i++){
-           int count = numSquares(n-i*i);
-           if(count!=Integer.MAX_VALUE) minCount = Math.min(minCount,count+1);
+        if(isPerfectSquare(n))return 1;
+        if(n<4)return n;
+        int size = 0;
+        for(int i=n-1;i>0;i--){
+            if(isPerfectSquare(i)){
+                size++;
+            }
         }
-        map.put(n,minCount);
-        return minCount;
+        int[] nums = new int[size];
+        int j=0;
+        for(int i=n-1;i>0;i--){
+            if(isPerfectSquare(i)){
+                nums[j]=i;
+                j++;
+            }
+        }
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+       return square(n,nums,0,dp)-1;
+    //    return res;
+    }
+
+    public int square(int n,int[] nums,int count,int[] dp){
+        if(n==0){
+            res = Math.min(res,count);
+            return 1;
+        }
+        if(n<0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        dp[n]=count;
+        int min = Integer.MAX_VALUE;
+        for(int j=0;j<nums.length;j++){
+           int c = square(n-nums[j],nums,count+1,dp);
+           if(c!=0){
+            min = Math.min(c,min);
+           }
+        }
+        return dp[n] = (min!=Integer.MAX_VALUE ? min+1 : 0);
+    }
+
+    public boolean isPerfectSquare(int n){
+        double num = Math.sqrt(n);
+        return Math.floor(num)==Math.ceil(num);
     }
 }
+
