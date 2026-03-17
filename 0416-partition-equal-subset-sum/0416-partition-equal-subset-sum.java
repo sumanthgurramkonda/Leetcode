d40e0526b8c7b0e8f3b98bc26ff57a7ed0fed69e
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-03-17 22:46:39 +0530

    Time: 110 ms (10.49%), Space: 66.8 MB (39.62%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,42 +1,26 @@
 class Solution {
+    int total = 0;
+    Boolean[][] dp;
     public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        for(int num : nums)total+=num;
 
-        int sum = 0;
-        for(int n : nums)sum+=n;
-        if(sum%2==1)return false;
-        sum /=2;
-        Boolean[][] dp = new Boolean[nums.length+1][sum+1];
-        return canPartition(nums,0,sum,dp);
-        // return canPartition(nums,sum);
+        dp = new Boolean[n][(total/2)+1];
+
+        if(total%2==1)return false;
+        return dfs(nums,0,0);
     }
 
-    public boolean canPartition(int[] nums, int index, int sum,Boolean[][] dp){
-        if(sum==0)return true;
-        if(sum<0 || index>=nums.length) return false;
-        if(dp[index][sum]!=null)return (boolean)dp[index][sum];
-        return dp[index][sum]= canPartition(nums,index+1,sum-nums[index],dp) || 
-                               canPartition(nums,index+1,sum,dp);
-    }
+    public boolean dfs(int[]nums, int index, int sum){
+        if(index==nums.length)return false;
+        if(total==(sum*2))return true;
+        if((sum*2)>total) return false;
+        if(dp[index][sum]!=null)return dp[index][sum];
 
-    public boolean canPartition(int[] nums,int sum){
-        int len = nums.length;
-        int length = (int)Math.pow(2,len-1)*len;
-        if(length<0)length = len;
-        List<Integer> list = new ArrayList(length);
-        for(int n:nums){
-            list.add(n);
-            for(int i=0;i<list.size();i++){
-                int num = list.get(i)+n;
-                if(num==sum)return true;
-                list.add(num);
-            }
-            
-        }
-        return false;
-    }
+        boolean skip = dfs(nums,index+1,sum);
+        boolean include = dfs(nums,index+1,sum+nums[index]);
 
+        return dp[index][sum]=skip||include;
+    }
 
 }
-
-
-
