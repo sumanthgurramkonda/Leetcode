501337970067fad551016ae052ae7be28a712ab8
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-03-19 19:17:05 +0530

    Time: 9 ms (55.06%), Space: 47 MB (87.19%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,38 +1,42 @@
 class Solution {
-    int[][] dp;
-    int n,m;
+    int[][] memo;
+    boolean[][] visited;
     public int longestIncreasingPath(int[][] matrix) {
-        n = matrix.length;
-        m = matrix[0].length;
-        dp = new int[n][m];
-        int result = 0;
+        int n = matrix.length, m = matrix[0].length;
+
+        visited = new boolean[n][m];
+        memo = new int[n][m];
+
+        for(int[] arr : memo) Arrays.fill(arr,-1);
+
+        int maxPathSize = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                result = Math.max(result,longestIncreasingPath(matrix,i,j));
+                maxPathSize = Math.max(maxPathSize, dfs(matrix, i,j));
             }
         }
-        return result;
+        return maxPathSize;
     }
 
-    public int longestIncreasingPath(int[][] matrix, int i ,int j){
-        if(i>=n || j>=m)return 0;
-        if(dp[i][j]!=0)return dp[i][j];
-        int left = 0;
-        int right = 0;
-        int top = 0;
-        int down = 0;
-        if(j-1>=0 && matrix[i][j] > matrix[i][j-1]){ 
-           left = longestIncreasingPath(matrix,i,j-1);
+
+    int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    public int dfs(int[][] matrix, int row, int col){
+
+        if(visited[row][col])return 0;
+        if(memo[row][col]!=-1)return memo[row][col];
+
+        int maxPathSize = 0;
+
+        visited[row][col] = true;
+        for(int[] dir : directions){
+            int r = row+dir[0];
+            int c = col+dir[1];
+            if(r<0 || r>=matrix.length || c<0 || c >= matrix[0].length || matrix[row][col]>=matrix[r][c]) continue;
+            maxPathSize = Math.max(maxPathSize, dfs(matrix, r,c));
         }
-        if(j+1<m && matrix[i][j] > matrix[i][j+1]){ 
-           right = longestIncreasingPath(matrix,i,j+1);
-        }
-        if(i-1>=0 && matrix[i][j] > matrix[i-1][j]){ 
-           top = longestIncreasingPath(matrix,i-1,j);
-        }
-        if(i+1<n && matrix[i][j] > matrix[i+1][j]){ 
-           down = longestIncreasingPath(matrix,i+1,j);
-        }
-        return dp[i][j] = Math.max(left,Math.max(right,Math.max(top,down)))+1;
+        visited[row][col]=false;
+
+        return memo[row][col]=maxPathSize+1;
     }
 }
