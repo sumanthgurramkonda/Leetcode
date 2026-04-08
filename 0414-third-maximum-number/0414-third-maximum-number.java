46c113b3cd147c0654004e80875ccf1c2d9b9477
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-04-09 00:16:42 +0530

    Time: 1 ms (99.16%), Space: 44.6 MB (85.94%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,18 +1,10 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        int firstLarge = Integer.MIN_VALUE;
-        int secondLarge = Integer.MIN_VALUE;
-        int thirdLarge = Integer.MIN_VALUE;
-        Set<Integer> set = new HashSet<>();
-        int i = 0;
-        for(int num : nums){
-            if(set.contains(num))continue;
-            nums[i++] = num;
-            set.add(num);
-        }
-        int n = set.size();
+        long firstLarge = Long.MIN_VALUE;
+        long secondLarge = Long.MIN_VALUE;
+        long thirdLarge = Long.MIN_VALUE;
 
-        for(i=0;i<n;i++){
+        for(int i=0;i<nums.length;i++){
             if(nums[i]==firstLarge || nums[i]==secondLarge || nums[i]==thirdLarge)continue;
             if(nums[i]>firstLarge){
                 thirdLarge = secondLarge;
@@ -25,7 +17,6 @@ class Solution {
                 thirdLarge = nums[i];
             }
         }
-        if(n<3)return firstLarge;
-        return thirdLarge;
+        return thirdLarge==Long.MIN_VALUE ? (int)firstLarge : (int)thirdLarge;
     }
 }
