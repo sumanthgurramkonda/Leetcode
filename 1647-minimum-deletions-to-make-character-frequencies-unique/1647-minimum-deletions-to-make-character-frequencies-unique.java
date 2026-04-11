caddea239700c2a1d539da896c23cb4d3c030acb
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-04-11 14:09:09 +0530

    Time: 16 ms (36.81%), Space: 48 MB (11.1%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minDeletions(String s) {
+        Integer[] freq = new Integer[26];
+        Arrays.fill(freq,0);
+        for(char c : s.toCharArray()) freq[c-'a']++;
+        Arrays.sort(freq, Collections.reverseOrder());
+        int count = 0;
+        for(int i=1;i<26;i++){
+            if(freq[i]==0)break;
+            if(freq[i]>=freq[i-1]){
+                int temp = freq[i];
+                freq[i] = Math.max(0,freq[i-1]-1);
+                count += (temp-freq[i]);
+            }
+        }
+        return count;
+    }
+}
+
+/*
+a - 2
+b - 2
+c - 2;
+*/
+
