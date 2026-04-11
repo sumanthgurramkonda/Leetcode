d06a12c76c26573cf67e903d007dc50f53c20511
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-04-11 14:16:32 +0530

    Time: 16 ms (36.81%), Space: 47.8 MB (53.39%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -3,17 +3,29 @@ class Solution {
         Integer[] freq = new Integer[26];
         Arrays.fill(freq,0);
         for(char c : s.toCharArray()) freq[c-'a']++;
-        Arrays.sort(freq, Collections.reverseOrder());
-        int count = 0;
-        for(int i=1;i<26;i++){
-            if(freq[i]==0)break;
-            if(freq[i]>=freq[i-1]){
-                int temp = freq[i];
-                freq[i] = Math.max(0,freq[i-1]-1);
-                count += (temp-freq[i]);
+        // // Arrays.sort(freq, Collections.reverseOrder());
+        // int count = 0;
+        // for(int i=1;i<26;i++){
+        //     if(freq[i]==0)break;
+        //     if(freq[i]>=freq[i-1]){
+        //         int temp = freq[i];
+        //         freq[i] = Math.max(0,freq[i-1]-1);
+        //         count += (temp-freq[i]);
+        //     }
+        // }
+        // return count;
+        Set<Integer> set = new HashSet<>();
+        int deletionCount = 0;
+        for(int f : freq){
+            while(f>0 && set.contains(f)){
+                deletionCount++;
+                f--;
+            }
+            if(f>0){
+                set.add(f);
             }
         }
-        return count;
+        return deletionCount;
     }
 }
 
