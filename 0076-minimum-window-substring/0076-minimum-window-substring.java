9c11632e16dbfb6c4d0e23645f408941cb0d0069
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-06-21 16:44:54 +0530

    Time: 2 ms (99.82%), Space: 45.6 MB (77.54%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -3,24 +3,24 @@ class Solution {
     public String minWindow(String s, String t) {
         int n = s.length(), m = t.length();
         if(n<m) return "";
-        int[] sFreq = new int[128];
-        int[] tFreq = new int[128];
-        for(char c : t.toCharArray()) tFreq[c]++;
+        int[] sFreq = new int[52];
+        int[] tFreq = new int[52];
+        for(char c : t.toCharArray()) tFreq[getIndex(c)]++;
 
         int required = 0, formed = 0;
-        for(int i=0;i<126;i++){
+        for(int i=0;i<52;i++){
             if(tFreq[i]>0)required++;
         }
         int left = 0;
         int l = 0, r = Integer.MAX_VALUE;
         for(int right=0; right<n; right++){
-            char c = s.charAt(right);
+            int c = getIndex(s.charAt(right));
             sFreq[c]++;
             if(tFreq[c]>0 && sFreq[c]==tFreq[c]){
                 formed++;
             } 
             while(left<=right && formed>=required){
-                char ch = s.charAt(left);
+                int ch = getIndex(s.charAt(left));
                 if(tFreq[ch]>0){
                     if(r-l >  right-left){
                         l = left;
@@ -38,15 +38,10 @@ class Solution {
         return r==Integer.MAX_VALUE ? "" : s.substring(l, r+1);
 
     }
-
-
-
-    public boolean isValidString(int[] sFreq, int[] tFreq, String t){
-
-        for(char c : t.toCharArray()){
-            if(tFreq[c]>0 && sFreq[c]<tFreq[c])return false;
-        }
-        return true;
+    
+    public int getIndex(char c){
+        if(c>='a' && c<='z') return c-'a';
+        return c-'A'+26;
     }
 
 }
