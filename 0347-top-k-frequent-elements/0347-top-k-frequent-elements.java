a28fe65607d6ea8797b9166771b31b14777ea8c3
Sumanth Gurramkonda <157909314+sumanthgurramkonda@users.noreply.github.com>
2026-06-21 13:45:43 +0530

    Time: 15 ms (55.5%), Space: 47.8 MB (31.79%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -5,15 +5,17 @@ class Solution {
         for(int i=0;i<n;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        Queue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((e1,e2)->{
-            int v1 = e1.getValue();
-            int v2 = e2.getValue();
-            if(v1==v2)return 0;
-            else if(v1>v2) return -1;
-            else return 1;
-            });   
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((e1, e2)->Integer.compare(e1.getValue(), e2.getValue()));   
         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            queue.offer(entry);
+            if(queue.size()==k){
+                if(queue.peek().getValue()<entry.getValue()){
+                    queue.poll();
+                    queue.offer(entry);
+                }
+            }else{
+                queue.offer(entry);
+            }
+            
         }
         int[] res = new int[k];
         for(int i=0;i<k;i++){
